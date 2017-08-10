29b5ec54521cc7ba50a9726f1a4dfeef13b2ce44
mkartsev86 <30770754+mkartsev86@users.noreply.github.com>
2017-08-10 06:47:06 +0300

    06:47

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
     define ('DEFAULT_LAYOUT', 'default_layout.php');
     ini_set('display_errors', 1);
-echo "попали сюда";
-exit;
-    
     require_once 'application/autoloader.php'; // подключаем автозагрузчик
     require_once 'application/bootstrap.php'; // инициируем и запускаем роутер
 ?>
